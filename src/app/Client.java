cc67d170bef411e1d8e573d9b7f233c3a28add64
Josh <Josh@DESKTOP-V7DBLS1.modem>
2020-09-21 23:19:59 +1000

    Functioning frame

--- src/app/Client.java
+++ src/app/Client.java
@@ -1,11 +1,15 @@
 package app;
 
+import model.Engine;
+import view.Frame;
+
 public class Client
 {
 
 	public static void main(String[] args)
 	{
-		
+		Engine engine = new Engine();
+		new Frame(engine);
 	}
 
 }
--- src/view/Frame.java
+++ src/view/Frame.java
@@ -2,8 +2,17 @@ package view;
 
 import javax.swing.JFrame;
 
+import model.Engine;
+
 @SuppressWarnings("serial")
 public class Frame extends JFrame
 {
-
+	public Frame(Engine engine)
+	{
+		super("Kris Kringle Presents");
+		
+		setBounds(420, 220, 650, 460);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
 }
