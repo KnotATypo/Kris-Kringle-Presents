6614f1ad3445798ef1c165f2ac2ea1592e33311f
Josh <Josh@DESKTOP-V7DBLS1.modem>
2020-09-21 23:19:27 +1000

    First implementation of Engine

--- src/model/Engine.java
+++ src/model/Engine.java
@@ -1,6 +1,24 @@
 package model;
 
+import java.util.Map;
+
 public class Engine
 {
+	private Map<String, Family> families;
 	
+	public void newFamily(String name)
+	{
+		families.put(name, new Family());
+	}
+	
+	public void addMember(String familyName, String member)
+	{
+		Family family = families.get(familyName);
+		family.addMember(member);
+	}
+	
+	public Map<String, Family> getFamilies()
+	{
+		return families;
+	}
 }
