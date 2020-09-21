32b18697356a72a646f86644fe898054ff613e04
Josh <Josh@DESKTOP-V7DBLS1.modem>
2020-09-21 22:55:12 +1000

    Basic classes

---
+++ src/app/Client.java
@@ -0,0 +1,11 @@
+package app;
+
+public class Client
+{
+
+	public static void main(String[] args)
+	{
+		
+	}
+
+}
---
+++ src/view/Frame.java
@@ -0,0 +1,9 @@
+package view;
+
+import javax.swing.JFrame;
+
+@SuppressWarnings("serial")
+public class Frame extends JFrame
+{
+
+}
