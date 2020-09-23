7c9c2144bbfeb5721225c21e029014d388708d87
Josh <Josh@DESKTOP-V7DBLS1.modem>
2020-09-24 01:38:49 +1000

    Created "test" case

--- src/app/Client.java
+++ src/app/Client.java
@@ -9,8 +9,39 @@ public class Client
 	public static void main(String[] args)
 	{
 		Engine engine = new Engine();
+		
+		addTest(engine);
 
-		new Frame(engine);
+		Frame frame = new Frame(engine);
+		
+		frame.updateData(false);
+	}
+
+	private static void addTest(Engine engine)
+	{
+		engine.newFamily("Dans");
+		engine.newFamily("McKie");
+		engine.newFamily("Mackay");
+		engine.newFamily("Grandma");
+		engine.newFamily("Murphy");
+		
+		engine.addMember("Dans", "Toby");
+		engine.addMember("Dans", "Maddy");
+		engine.addMember("Dans", "Cath");
+		engine.addMember("Dans", "Geoff");
+		
+		engine.addMember("Mackay", "Josh");
+		engine.addMember("Mackay", "Sam");
+		engine.addMember("Mackay", "Barb");
+		engine.addMember("Mackay", "Pete");
+		
+		engine.addMember("McKie", "Andy");
+		engine.addMember("McKie", "Sandra");
+		
+		engine.addMember("Murphy", "Jen");
+		engine.addMember("Murphy", "Bryan");
+		
+		engine.addMember("Grandma", "Grandma");
 	}
 
 }
